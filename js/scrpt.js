6ac220a14fc8241339b55ff38e65bd3d91768fd0
khadar cadare <cadar@MacBook-Air.local>
2023-04-02 11:11:21 +0300

    chaged heading home page

---
+++ js/scrpt.js
@@ -0,0 +1,7 @@
+const cursor = document.querySelector(".cursor");
+document.body.addEventListener("mousemove", ({
+    clientX,
+    clientY
+}) => {
+    cursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+});
